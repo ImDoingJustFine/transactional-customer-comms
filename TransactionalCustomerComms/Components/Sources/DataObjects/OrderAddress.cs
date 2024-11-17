c7ea17898e4f5241a28d566c094492da0191628c
ImDoingJustFine <xandercolby@outlook.com>
2024-11-17 15:46:50 -0600

    beginning of first data object

---
+++ TransactionalCustomerComms/Components/Sources/DataObjects/OrderAddress.cs
@@ -0,0 +1,16 @@
+namespace TransactionalCustomerComms.Components.Sources.DataObjects;
+
+public class OrderAddress()
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Company { get; set; }
+    public required string StreetOne { get; set; }
+    public required string StreetTwo { get; set; }
+    public required string City { get; set; }
+    public required string State { get; set; }
+    public required string ZipCode { get; set; }
+    public required string Country { get; set; }
+    public required string Phone { get; set; }
+    public required string Email { get; set; }
+}
